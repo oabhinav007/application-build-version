ac5510d89059909af7d44e516be0308ee892f3df
Abhinav Oruganti <abhinav.oruganti@effigo.in>
2023-03-12 18:51:41 +0530

    modified-files

--- src/main/java/zama/training/StartWebApplication.java
+++ src/main/java/zama/training/StartWebApplication.java
@@ -8,14 +8,34 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class StartWebApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SpringApplication.run(StartWebApplication.class, args);
-        String version = System.getenv("APP_VERSION");
-        System.out.println("Application version: " + version);
-    }
+        //System.out.println("args.length="+args.length);
+        String version = args[0];
+        //System.getenv("app.version");
+        if (version == null || version.isEmpty()) {
+            throw new IllegalArgumentException("Missing value for APP_VERSION");
+        }
+        System.out.println(version);
+        
+    }*/
+    // public static void main(String[] args) {
+    //     String buildNumber = System.getProperty("BUILD_NUMBER");
+    //     System.out.println("Build number: " + buildNumber);
+    //     // Use buildNumber in your program
+    // }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(StartWebApplication.class);
+    public static void main(String[] args) {
+        String buildNumber = System.getenv("BUILD_NUMBER");
+        System.out.println("Build number: " + buildNumber);
+
     }
+    
+    
+
+    // @Override
+    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        
+    //     return builder.sources(StartWebApplication.class);
+    // }
 }
--- src/test/java/zama/training/SpringMvcExampleWarApplicationTests.java
+++ src/test/java/zama/training/SpringMvcExampleWarApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringMvcExampleWarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
