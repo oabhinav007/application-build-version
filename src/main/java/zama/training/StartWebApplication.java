61e93d41a640778bd405480095785d05a24859de
Abhinav Oruganti <abhinav.oruganti@effigo.in>
2023-03-13 12:11:24 +0530

    modified code

--- src/main/java/zama/training/StartWebApplication.java
+++ src/main/java/zama/training/StartWebApplication.java
@@ -8,34 +8,20 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class StartWebApplication extends SpringBootServletInitializer {
 
-    /*public static void main(String[] args) {
-        SpringApplication.run(StartWebApplication.class, args);
-        //System.out.println("args.length="+args.length);
-        String version = args[0];
-        //System.getenv("app.version");
-        if (version == null || version.isEmpty()) {
-            throw new IllegalArgumentException("Missing value for APP_VERSION");
-        }
-        System.out.println(version);
-        
-    }*/
-    // public static void main(String[] args) {
-    //     String buildNumber = System.getProperty("BUILD_NUMBER");
-    //     System.out.println("Build number: " + buildNumber);
-    //     // Use buildNumber in your program
-    // }
-
     public static void main(String[] args) {
-        String buildNumber = System.getenv("BUILD_NUMBER");
-        System.out.println("Build number: " + buildNumber);
-
+    String buildVersion = System.getenv("BUILD_NUMBER");
+    if (buildVersion == null || buildVersion.isEmpty()) {
+        throw new IllegalArgumentException("Missing value for BUILD_NUMBER");
     }
-    
-    
+    System.out.println("Build version: " + buildVersion);
 
-    // @Override
-    // protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    SpringApplication.run(StartWebApplication.class, args);
+}
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         
-    //     return builder.sources(StartWebApplication.class);
-    // }
+        return builder.sources(StartWebApplication.class);
+    }
 }
